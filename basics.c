267abf30759b8f0137ce55419749ed420b072452
jasmeett09 <jasmeetkaur0946@gmail.com>
2025-01-20 14:41:29 +0530

    Add files via upload

---
+++ basics.c
@@ -0,0 +1,96 @@
+
+/*
+void number(int num){
+	if (num%2==0){
+		printf("even");
+	}
+	else{
+		printf("odd");
+	}
+}
+void main(){
+    int num;
+	printf("enter a number:");
+	scanf("%d",&num);
+	number(num);
+}
+*/
+/*
+void max(int a,int b,int c){
+	if (a>b & a>c){
+		printf("a is max");
+	}
+	else if (b>a & b>c){
+		printf("b is max");
+	}
+	else {
+		printf("c is max");
+	}
+}
+void main(){
+	int a,b,c;
+	printf("enter a:");
+	scanf("%d",&a);
+	printf("enter b:");
+	scanf("%d",&b);
+	printf("enter c:");
+	scanf("%d",&c);
+	max(a,b,c);
+}
+
+void swap(int a,int b){
+          a=b;
+    
+}
+void main(){
+	int a,b,c;
+	printf("enter a:");
+	scanf("%d",&a);
+	printf("enter b:");
+	scanf("%d",&b);
+	
+}
+*/
+
+#include<stdio.h>
+int main()
+{
+  float c= 2E-4f; // hexadecimal value 
+  printf("%f",c);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
